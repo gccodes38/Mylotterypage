473199c6a63e4366ece82b364d246b3aca9cb350
gccodes38 <127054630+gccodes38@users.noreply.github.com>
2023-04-01 21:38:28 +0100

    Update mylottery.py

--- mylottery/mylottery.py
+++ mylottery/mylottery.py
@@ -1,19 +1,20 @@
 import random
 
-
 def yourname_password_gen():
+    
 
-# Generate a random lottery number
-    name = input("Enter user's name: ")
-    lucky_numbercode = int(input("input your lucky number: "))
-    rand_number = random.randint(1, 50)
-    #print(rand_number)
-    # winninglottery_num = "30"
-    for x in range(1):
-        if lucky_numbercode == 30:
-            print("congratulations", name, "your lottery number is 30, you are the lucky winner")
-        else:
-            print("sorry", name, "check some other time")
+#Generate a random lottery number
+    
+    name = input("Enter Username: ")
+    lottery_number = int(input("Enter lottery number: "))
+    rand = random.sample(range(0,50), k=1)
+    winning_number = 30
 
+    if lottery_number > 50:
+        print("syntax error")
+    elif lottery_number == winning_number and lottery_number == rand:
+        print("congratulations", name, "your lucky number is", winning_number, "you are the lucky winner")
+    else:
+        print("sorry", name, "your lottery number is", lottery_number, "you got", rand, "you failed, try again next time")
 
 yourname_password_gen()
